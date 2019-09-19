7870fb70fda3845c53137c6768a80b6f07ff1e85
Muppet <811583783@qq.com>
2019-09-19 18:48:47 +0800

    初始化

--- app/src/main/java/com/muppet/weather/Activity/MainActivity.java
+++ app/src/main/java/com/muppet/weather/Activity/MainActivity.java
@@ -13,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //1234
+
+
     }
 }
---
+++ app/src/main/java/com/muppet/weather/Utils/Constant.java
@@ -0,0 +1,6 @@
+package com.muppet.weather.Utils;
+
+public class Constant {
+
+    public static final String TEST = "test";
+}
