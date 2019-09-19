2b60d2c3b023a6babbdb28cf1b8c62d4eee25ba8
Muppet <811583783@qq.com>
2019-09-19 18:22:40 +0800

    初始化

--- app/src/main/java/com/muppet/weather/App.java
+++ app/src/main/java/com/muppet/weather/App.java
@@ -6,11 +6,17 @@ import org.xutils.x;
 
 public class App extends Application {
 
+    public static Application application;
     @Override
     public void onCreate() {
         super.onCreate();
+        application = this;
         x.Ext.init(this);
         //输出debug日志
         x.Ext.setDebug(true);
+    }
+    //获取项目上下文
+    public static Application getAppContext() {
+        return application;
     }
 }
