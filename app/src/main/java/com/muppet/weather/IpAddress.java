7483decaa9ffac31b7051b8bd71ddc852ba3938b
Muppet <811583783@qq.com>
2019-09-19 11:58:25 +0800

    初始化

---
+++ app/src/main/java/com/muppet/weather/IpAddress.java
@@ -0,0 +1,22 @@
+package com.muppet.weather;
+
+import androidx.annotation.NonNull;
+
+public class IpAddress {
+
+    public static final String Server_IP = "http://192.168.0.51:8019";//肖卓然
+
+    public static boolean isDebug = false ; //是否为调试模式
+
+    public static String getUrl(@NonNull String url) {
+
+        StringBuilder stringBuilder = new StringBuilder(Server_IP);
+        if (!url.startsWith("/")) {
+            stringBuilder.append("/");
+        }
+        stringBuilder.append(url);
+        return stringBuilder.toString();
+    }
+    //登录
+    public static final String LOGIN = "/apiuser/login";
+}
