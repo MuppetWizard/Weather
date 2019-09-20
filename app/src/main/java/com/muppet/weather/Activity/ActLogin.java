b684d1f3ada04f65183278ee7be2385de0ec4375
Muppet <811583783@qq.com>
2019-09-20 11:58:17 +0800

    初始化

---
+++ app/src/main/java/com/muppet/weather/Activity/ActLogin.java
@@ -0,0 +1,16 @@
+package com.muppet.weather.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.muppet.weather.R;
+
+public class ActLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_login);
+    }
+}
