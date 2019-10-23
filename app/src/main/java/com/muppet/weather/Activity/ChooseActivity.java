e85180b57d68b7323d398f97787b8339735c16de
zhoupeng <1721322410@qq.com>
2019-10-23 10:10:32 +0800

    1263

--- app/src/main/java/com/muppet/weather/Activity/ChooseActivity.java
+++ app/src/main/java/com/muppet/weather/Activity/ChooseActivity.java
@@ -1,13 +1,8 @@
 package com.muppet.weather.Activity;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -23,15 +18,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import com.bumptech.glide.Glide;
 import com.muppet.weather.BuildConfig;
+import com.muppet.weather.Model.MessageEvent;
 import com.muppet.weather.R;
 import com.muppet.weather.Utils.FileUtil;
 import com.muppet.weather.View.CircleImageView;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
 import org.xutils.x;
@@ -48,7 +54,7 @@ import butterknife.OnClick;
 
 import static com.muppet.weather.Utils.FileUtil.getRealFilePathFromUri;
 
-public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnClickListener {
+public class ChooseActivity extends AppCompatActivity implements View.OnClickListener {
 
     //请求相机
     private static final int REQUEST_CAPTURE = 100;
@@ -60,46 +66,81 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
     private static final int READ_EXTERNAL_STORAGE_REQUEST_CODE = 103;
     //请求写入外部存储
     private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 104;
+
+    @BindView(R.id.go_myactivity)
+    ImageView goMyactivity;
+    @BindView(R.id.chang_nickname)
+    LinearLayout changNickname;
+    @BindView(R.id.chang_sex)
+    LinearLayout changSex;
+    @BindView(R.id.chang_autograph)
+    LinearLayout changAutograph;
+    @BindView(R.id.my_info_nickname)
+    TextView myInfoNickname;
+    @BindView(R.id.my_info_sex)
+    TextView myInfoSex;
+    @BindView(R.id.my_info_auto)
+    TextView myInfoAuto;
+    @BindView(R.id.head_image)
+    CircleImageView headImage1;
+    @BindView(R.id.chang_icon)
+    LinearLayout changIcon;
+
     //调用照相机返回图片文件
     private File tempFile;
+    // 1: qq, 2: weixin
     private int type;
 
-    @BindView(R.id.go_back)
-    ImageView goBack;
-    @BindView(R.id.show_faces)
-    CircleImageView showFaces;
-    @BindView(R.id.rl_modify_icon)
-    RelativeLayout rlModifyIcon;
-    @BindView(R.id.show_nickname)
-    TextView showNickname;
-    @BindView(R.id.rl_modify_nickname)
-    RelativeLayout rlModifyNickname;
-    @BindView(R.id.show_sex)
-    TextView showSex;
-    @BindView(R.id.rl_modify_sex)
-    RelativeLayout rlModifySex;
-    @BindView(R.id.show_birthday)
-    TextView showBirthday;
-    @BindView(R.id.rl_modify_birthday)
-    RelativeLayout rlModifyBirthday;
+    private SharedPreferences pref;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_modify_my_info);
+        setContentView(R.layout.activity_choose);
         ButterKnife.bind(this);
-        rlModifyIcon.setOnClickListener(this);
+        changIcon.setOnClickListener(this);
+
+        EventBus.getDefault().register(this);
+        initGetDate();
     }
 
+    private void initGetDate() {
+        pref = getSharedPreferences("Login", this.MODE_PRIVATE);
+        String nickname = pref.getString("nickname", "");
+        String autograph = pref.getString("autograph", "");
+        String avatar = pref.getString("avatar", "");
+        String sex = pref.getString("sex", "");
+        if (avatar == ""){
+            headImage1 = findViewById(R.id.head_image);
+        }else {
+            Glide.with(this).load("http://192.168.10.237:8080/EndProject/"+avatar).into(headImage1);
+        }
+        myInfoNickname.setText(nickname);
+        myInfoAuto.setText(autograph);
+        myInfoSex.setText(sex);
+    }
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.rl_modify_icon:
+            case R.id.chang_icon:
                 type = 1;
                 uploadHeadImage();
                 break;
         }
     }
 
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void Envet(MessageEvent messageEvent){
+        boolean success = messageEvent.isSuccess();
+        initGetDate();
+    }
+
+
+    /**
+     * 上传头像
+     */
     private void uploadHeadImage() {
         View view = LayoutInflater.from(this).inflate(R.layout.layout_popupwindow, null);
         TextView btnCarema = view.findViewById(R.id.btn_camera);
@@ -126,10 +167,10 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
             @Override
             public void onClick(View v) {
                 //权限判断
-                if (ContextCompat.checkSelfPermission(ModifyMyInfoActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                if (ContextCompat.checkSelfPermission(ChooseActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
                     //申请WRITE_EXTERNAL_STORAGE权限
-                    ActivityCompat.requestPermissions(ModifyMyInfoActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    ActivityCompat.requestPermissions(ChooseActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                             WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
                 } else {
                     //跳转到调用系统相机
@@ -142,10 +183,10 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
             @Override
             public void onClick(View v) {
                 //权限判断
-                if (ContextCompat.checkSelfPermission(ModifyMyInfoActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                if (ContextCompat.checkSelfPermission(ChooseActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
                     //申请READ_EXTERNAL_STORAGE权限
-                    ActivityCompat.requestPermissions(ModifyMyInfoActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    ActivityCompat.requestPermissions(ChooseActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                             READ_EXTERNAL_STORAGE_REQUEST_CODE);
                 } else {
                     //跳转到相册
@@ -162,6 +203,11 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
         });
     }
 
+
+    /**
+     * 外部存储权限申请返回
+     */
+    @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
@@ -177,6 +223,10 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
         }
     }
 
+
+    /**
+     * 跳转到相册
+     */
     private void gotoPhoto() {
         Log.d("evan", "*****************打开图库********************");
         //跳转到调用系统图库
@@ -184,6 +234,10 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
         startActivityForResult(Intent.createChooser(intent, "请选择图片"), REQUEST_PICK);
     }
 
+
+    /**
+     * 跳转到照相机
+     */
     private void gotoCamera() {
         Log.d("evan", "*****************打开相机********************");
         //创建拍照存储的图片文件
@@ -194,7 +248,7 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             //设置7.0中共享文件，分享路径定义在xml/file_paths.xml
             intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-            Uri contentUri = FileProvider.getUriForFile(ModifyMyInfoActivity.this, BuildConfig.APPLICATION_ID + ".fileProvider", tempFile);
+            Uri contentUri = FileProvider.getUriForFile(ChooseActivity.this, BuildConfig.APPLICATION_ID + ".fileProvider", tempFile);
             intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
         } else {
             intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
@@ -202,6 +256,7 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
         startActivityForResult(intent, REQUEST_CAPTURE);
     }
 
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
         switch (requestCode) {
@@ -225,7 +280,7 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
                     String cropImagePath = getRealFilePathFromUri(getApplicationContext(), uri);
                     Bitmap bitMap = BitmapFactory.decodeFile(cropImagePath);
                     if (type == 1) {
-                        showFaces.setImageBitmap(bitMap);
+                        headImage1.setImageBitmap(bitMap);
                     }
                     //此处后面可以将bitMap转为二进制上传后台网络
                     Random r = new Random();
@@ -280,6 +335,9 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
         return myCaptureFile;
     }
 
+    /**
+     * 打开截图界面
+     */
     public void gotoClipActivity(Uri uri) {
         if (uri == null) {
             return;
@@ -291,22 +349,29 @@ public class ModifyMyInfoActivity extends AppCompatActivity implements View.OnCl
         startActivityForResult(intent, REQUEST_CROP_PHOTO);
     }
 
-    @OnClick({R.id.go_back, R.id.rl_modify_nickname, R.id.rl_modify_sex, R.id.rl_modify_birthday})
+    @OnClick({R.id.go_myactivity, R.id.chang_nickname, R.id.chang_sex, R.id.chang_autograph})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.go_back:
-                Intent intent = new Intent();
-                intent.putExtra("goback", "返回");
-                intent.setClass(this, MainActivity.class);
-                startActivity(intent);
+            case R.id.go_myactivity:
                 finish();
                 break;
-            case R.id.rl_modify_nickname:
+            case R.id.chang_nickname:
+                //startActivity(new Intent(this, ModifyNicknameActivity.class));
                 break;
-            case R.id.rl_modify_sex:
+            case R.id.chang_sex:
+                //startActivity(new Intent(this, ModifySexActivity.class));
                 break;
-            case R.id.rl_modify_birthday:
+            case R.id.chang_autograph:
+                //startActivity(new Intent(this, ModifyAutographActivity.class));
                 break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (EventBus.getDefault().isRegistered(this)){
+            EventBus.getDefault().unregister(this);
         }
     }
 }
