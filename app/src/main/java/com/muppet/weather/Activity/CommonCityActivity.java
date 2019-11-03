4b93f3f7da869b17c12bf66305d9c2ed016b277b
Alluriest <1721322410@qq.com>
2019-11-03 10:34:38 +0800

    1263

--- app/src/main/java/com/muppet/weather/Activity/CommonCityActivity.java
+++ app/src/main/java/com/muppet/weather/Activity/CommonCityActivity.java
@@ -30,19 +30,12 @@ import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
 import org.xutils.x;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import okhttp3.Call;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class CommonCityActivity extends AppCompatActivity {
 
